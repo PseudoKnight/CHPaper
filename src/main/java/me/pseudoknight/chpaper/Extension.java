861a177e0ac10c932f79397a7ba05334e194b88c
Michael Smith <michael.jt.smith@gmail.com>
2021-08-19 14:15:04 -0700

    Use logger

--- src/main/java/me/pseudoknight/chpaper/Extension.java
+++ src/main/java/me/pseudoknight/chpaper/Extension.java
@@ -2,9 +2,14 @@ package me.pseudoknight.chpaper;
 
 import com.laytonsmith.PureUtilities.SimpleVersion;
 import com.laytonsmith.PureUtilities.Version;
+import com.laytonsmith.core.MSLog;
+import com.laytonsmith.core.Static;
+import com.laytonsmith.core.constructs.Target;
 import com.laytonsmith.core.extensions.AbstractExtension;
 import com.laytonsmith.core.extensions.MSExtension;
 import me.pseudoknight.chpaper.abstraction.paper.PaperListeners;
+
+import java.util.logging.Level;
 
 @MSExtension("CHPaper")
 public class Extension extends AbstractExtension {
@@ -16,12 +21,12 @@ public class Extension extends AbstractExtension {
 	@Override
 	public void onStartup() {
 		PaperListeners.register();
-		System.out.println("CHPaper " + getVersion() + " loaded.");
+		Static.getLogger().log(Level.INFO, "CHPaper " + getVersion() + " loaded.");
 	}
 
 	@Override
 	public void onShutdown() {
 		PaperListeners.unregister();
-		System.out.println("CHPaper " + getVersion() + " unloaded.");
+		Static.getLogger().log(Level.INFO, "CHPaper " + getVersion() + " unloaded.");
 	}
 }
